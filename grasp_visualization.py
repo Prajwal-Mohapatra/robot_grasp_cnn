90c79416004693b35424029ba1cb13db378de372
Prajwal Mohapatra <154001893+Prajwal-Mohapatra@users.noreply.github.com>
2025-06-17 19:36:52 +0530

    Update grasp_visualization.py

--- grasp_visualization.py
+++ grasp_visualization.py
@@ -8,9 +8,15 @@ model = GraspCNN()
 model.load_state_dict(torch.load("saved_models/grasp_cnn.pth"))
 model.eval()
 
-val_dataset = CornellGraspDataset(root="./data/cornell")
+val_dataset = CornellGraspDataset(root="./data/cornell-grasp")
 sample = val_dataset[0]
+
+print("RGB shape:", sample['rgb'].shape)
+print("Depth shape:", sample['depth'].shape)
+print("Grasp shape:", sample['grasp'].shape)
 
 with torch.no_grad():
     pred = model(sample['rgb'].unsqueeze(0), sample['depth'].unsqueeze(0))
+    print("Prediction shape:", pred.shape)
+    print("Prediction values:\n", pred[0])
     show_rgb_depth_grasps(sample['rgb'], sample['depth'], sample['grasp'], pred[0])
