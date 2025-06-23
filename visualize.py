a0eb3cfc7b92353a6397f35f61c7e140a4ef2920
Prajwal Mohapatra <154001893+Prajwal-Mohapatra@users.noreply.github.com>
2025-06-23 11:37:51 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,4 +1,6 @@
 # ===================== visualize.py =====================
+from matplotlib import legend
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -17,16 +19,23 @@ def show_rgb_depth_grasps(rgb, depth, grasp=None, pred_grasp=None, save_path=Non
     axs[1].imshow(depth, cmap='gray')
     axs[1].set_title("Depth")
 
-    if grasp is not None: #Ground truth
+    legend_handles = []
+
+    if grasp is not None:
         for g in grasp:
             g = g.numpy()
             g = np.vstack([g, g[0]])  # Close the grasp rectangle
             axs[0].plot(g[:, 0], g[:, 1], 'b-')
+        legend_handles.append(Line2D([0],[0], color = 'blue', lw=1, label='Ground Truth'))
 
-    if pred_grasp is not None: #Prediction rectangle
+    if pred_grasp is not None:
         pred_grasp = pred_grasp.detach().cpu().numpy()
         pred_grasp = np.vstack([pred_grasp, pred_grasp[0]])
         axs[0].plot(pred_grasp[:, 0], pred_grasp[:, 1], 'y-')
+        legend_handles.append(Line2D([0],[0], color = 'yellow', lw=1, label='Prediction'))
+
+    if legend_handles:
+      axs[0].legend(handles=legend_handles, loc='lower right')
 
     plt.tight_layout()
     if save_path:
